279983ca156267207a0653e9d52fab56cf911097
ShunsukeKanai <knisnsk@gmail.com>
2020-01-26 19:54:34 +0900

    validation　の追加で空つぶやきできなくした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(post_params)
-    post.save!
-    redirect_to posts_url, notice: "つぶやきました"
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "つぶやきました"
+    else
+      render :new
+    end
   end
 
   def edit
