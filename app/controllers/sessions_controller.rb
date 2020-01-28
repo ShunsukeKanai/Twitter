af2333d7aeed3e4b19900891d7f8bd1276176e8d
ShunsukeKanai <knisnsk@gmail.com>
2020-01-28 23:50:09 +0900

    ログアウト機能作成 #15

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user&.aunthentificate(session_params[:passeord])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'ログインしました'
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, notice: 'ログアウトしました。'
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
